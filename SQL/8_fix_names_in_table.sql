cc37dfaff7bad31b10d29318b187ac5706ca11b2
amyjaderichards <amyjaderichards@googlemail.com>
2022-11-10 09:21:18 +0000

    SQL problem solved

---
+++ SQL/8_fix_names_in_table.sql
@@ -0,0 +1,32 @@
+/*
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+
+Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+*/
+
+SELECT Users.user_id, CONCAT(UPPER(SUBSTR(Users.name,1,1)),LOWER(SUBSTR(Users.name,2))) AS name
+    FROM Users
+    ORDER BY Users.user_id ASC
+
+
+/*
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+*/
